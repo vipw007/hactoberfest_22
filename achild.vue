a58a4c45cacbfed770db70b466c08b2c74c60e64
vipviv <72593650+vipw007@users.noreply.github.com>
2022-10-03 09:06:32 +0530

    Add files via upload

---
+++ achild.vue
@@ -0,0 +1,35 @@
+<template>
+    
+    <div class="user">
+        <h2>{{data.name}}</h2>
+        <h2>{{data.email}}</h2>
+        <button v-on:click="getData(data.name)"> Alert Name</button>
+    </div>
+
+</template>
+
+<script>
+    export default{
+        name:'a-child',
+        props:
+        {
+            data: Object,
+            getData: Function
+        },
+        // methods:
+        // {
+        //     getdata()
+        //     {
+        //         alert("hello")
+        //     }
+        // }
+    }
+</script>
+
+<style>
+    .user{
+    background-color: deepskyblue;
+    padding: 10px;
+    border-bottom: 1px solid;
+    }
+</style>
---
+++ aparent.vue
@@ -0,0 +1,52 @@
+<template>
+<h1>Resue component</h1>
+<ul>
+    <li v-for="item in child" :key="item.name">
+    <achild :data ="item" :getData= "getData"/>
+    </li>
+</ul>
+
+</template>
+
+<script>
+import achild from './achild.vue'
+
+export default {
+    name: "1-parent",
+    components: {
+        achild
+    },
+    methods:{
+        getData(name)
+        {
+            alert(name)
+
+        }
+    },
+    data() {
+        return {
+            child: [
+
+                {
+                    name: 'vivek',
+                    email: 'vivek@gmail.com'
+                },
+                {
+                    name: 'vipul',
+                    email: 'vivek@gmail.com'
+                },
+                {
+                    name: 'vivek',
+                    email: 'vivek@gmail.com'
+                }
+            ]
+
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
